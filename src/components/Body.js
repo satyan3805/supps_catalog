40031e6442fc3a11cce0b9cb3df7122a581f96ef
satyan3805 <145639478+satyan3805@users.noreply.github.com>
2024-12-03 09:20:22 +0530

    files updated

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,10 +2,26 @@ import ProductDisplay from "./ProductDisplay";
 import FetchCSVData from "./FetchCSVData";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import {
+  CButton,
+  CCard,
+  CCollapse,
+  CCardBody,
+  CAccordion,
+  CAccordionItem,
+  CAccordionHeader,
+  CAccordionBody,
+} from "@coreui/react";
 
 const Body = () => {
-
   const [csvData, setCsvData] = useState([]);
+  const [protineCatData, setProtineCatData] = useState([]);
+  const [performanceCatData, setPerformanceCatData] = useState([]);
+  const [weightManagementCatData, setWeightManagementCatData] = useState([]);
+  const [healthAndVitaminsCatData, setHealthAndVitaminsCatData] = useState([]);
+  const [gainerCatData, setGainerCatData] = useState([]);
+
+  const [protieneCatVisible, setProtieneCatVisible] = useState(false);
 
   useEffect(() => {
     fetchCSVData(); // Fetch the CSV data when the component mounts
@@ -19,17 +35,17 @@ const Body = () => {
       .get(csvUrl) // Use Axios to fetch the CSV data
       .then((response) => {
         const parsedCsvData = parseCSV(response.data); // Parse the CSV data into an array of objects
-        parsedCsvData.map(item => {
-          const imgArr=[
-          "../images/supp_images/"+item.Image_group_id+"/1.webp",
-          "../images/supp_images/"+item.Image_group_id+"/2.webp",
-          "../images/supp_images/"+item.Image_group_id+"/3.webp"
+        parsedCsvData.map((item) => {
+          const imgArr = [
+            "../images/supp_images/" + item.Image_group_id + "/1.webp",
+            "../images/supp_images/" + item.Image_group_id + "/2.webp",
+            "../images/supp_images/" + item.Image_group_id + "/3.webp",
           ];
-          item['imgList']=imgArr;
-      
+          item["imgList"] = imgArr;
+
           return item;
-      })
-        
+        });
+
         setCsvData(parsedCsvData); // Set the fetched data in the component's state
         console.log(parsedCsvData); // Now you can work with 'csvData' in your component's state.
 
@@ -58,13 +74,16 @@ const Body = () => {
             }
           }
         }
-
+        setProtineCatData(ProtineCatArr);
+        setPerformanceCatData(PerformanceCatArr);
+        setWeightManagementCatData(WeightManagementCatArr);
+        setHealthAndVitaminsCatData(HealthAndVitaminsCatArr);
+        setGainerCatData(GainerCatArr);
         // console.log(ProtineCatArr);
         // console.log(PerformanceCatArr);
         // console.log(WeightManagementCatArr);
         // console.log(HealthAndVitaminsCatArr);
         // console.log(GainerCatArr);
-        
       })
       .catch((error) => {
         console.error("Error fetching CSV data:", error);
@@ -87,11 +106,69 @@ const Body = () => {
   }
 
   return (
-    <div className="body-div">
-        {csvData.map((prod) => (
-          
-          <ProductDisplay key={prod.Product_ID} prodInfo={prod}/>
-        ))}
+    <div>
+      <CAccordion alwaysOpen activeItemKey={1}>
+        <CAccordionItem itemKey={1}>
+          <CAccordionHeader className="accord-header">
+            <strong>Protienes</strong>
+          </CAccordionHeader>
+          <CAccordionBody className="accord-body">
+            <div className="body-div">
+              {protineCatData.map((prod) => (
+                <ProductDisplay key={prod.Product_ID} prodInfo={prod} />
+              ))}
+            </div>
+          </CAccordionBody>
+        </CAccordionItem>
+        <CAccordionItem itemKey={2}>
+          <CAccordionHeader className="accord-header">
+            <strong>Performance</strong>
+          </CAccordionHeader>
+          <CAccordionBody>
+            <div className="body-div">
+              {performanceCatData.map((prod) => (
+                <ProductDisplay key={prod.Product_ID} prodInfo={prod} />
+              ))}
+            </div>
+          </CAccordionBody>
+        </CAccordionItem>
+        <CAccordionItem itemKey={3}>
+          <CAccordionHeader className="accord-header">
+            <strong>Weight Management</strong>{" "}
+          </CAccordionHeader>
+          <CAccordionBody>
+            <div className="body-div">
+              {weightManagementCatData.map((prod) => (
+                <ProductDisplay key={prod.Product_ID} prodInfo={prod} />
+              ))}
+            </div>
+          </CAccordionBody>
+        </CAccordionItem>
+        <CAccordionItem itemKey={4}>
+          <CAccordionHeader className="accord-header">
+            <strong>Health and Vitamins</strong>
+          </CAccordionHeader>
+          <CAccordionBody>
+            <div className="body-div">
+              {healthAndVitaminsCatData.map((prod) => (
+                <ProductDisplay key={prod.Product_ID} prodInfo={prod} />
+              ))}
+            </div>
+          </CAccordionBody>
+        </CAccordionItem>
+        <CAccordionItem itemKey={5}>
+          <CAccordionHeader className="accord-header">
+            <strong>Gainers</strong>
+          </CAccordionHeader>
+          <CAccordionBody>
+            <div className="body-div">
+              {gainerCatData.map((prod) => (
+                <ProductDisplay key={prod.Product_ID} prodInfo={prod} />
+              ))}
+            </div>
+          </CAccordionBody>
+        </CAccordionItem>
+      </CAccordion>
     </div>
   );
 };
